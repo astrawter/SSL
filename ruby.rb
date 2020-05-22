d6ccbb53c7a23bfdcf0f309794bc4a7d1fcdb544
Alexandria <afstrawter@gmail.com>
2020-05-22 13:48:47 -0500

    update class method
    Working on getting the class method to function and get the grade

---
+++ ruby.rb
@@ -0,0 +1,45 @@
+# Create a class "Grader"
+# Create methods to output a valid grade
+# Using the command line ask user a question for students name
+# Using the command line ask user a question for assignment name
+# Using the command line ask user a question for grade in numbers
+# Return the letter grade for the assignment
+# Class Example
+class Grader
+  def initialize(name, aname, grade)
+    @name =  name
+    @aname = aname
+    @grade = grade
+  end
+  def getGrade
+    return @grade
+  end
+  end
+
+
+
+user = Grader.new("John", "Grader", 92)
+puts(user.getGrade())
+
+
+#   def getGrade
+#     if 0 > num:
+#         return "not a valid grade."
+#     elsif num < 60
+#       return "F"
+#     elsif num <= 69
+#       return "D"
+#     elsif num <= 79
+#       return "C"
+#     elsif num <= 89
+#       return "B"
+#     elsif num <= 100
+#       return "A"
+#     else
+#       return "not a valid grade."
+#     end
+#   end
+# end
+# end
+# end
+# end
