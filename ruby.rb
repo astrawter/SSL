af0613e3c3c2a7df7b7b5b5a16886310cfc225a9
Alexandria <afstrawter@gmail.com>
2020-05-22 14:05:19 -0500

    finshed getGrade method
    test variables for the grader method

--- ruby.rb
+++ ruby.rb
@@ -1,51 +1,41 @@
 # Create a class "Grader"
-# Create methods to output a valid grade
-# Using the command line ask user a question for students name
-# Using the command line ask user a question for assignment name
-# Using the command line ask user a question for grade in numbers
-# Return the letter grade for the assignment
-# Class Example
 class Grader
   def initialize(name, aname, grade)
     @name =  name
     @aname = aname
     @grade = grade
   end
+  # Create methods to output a valid grade
   def getGrade
     if 0 > @grade || @grade > 100
       return "not a valid grade."
     elsif @grade <= 60
       return "F"
+    elsif @grade <= 69
+      return "D"
+    elsif @grade <= 79
+      return "C"
+    elsif @grade <= 89
+      return "B"
     else
       return "A"
     end
   end
   end
 
+# Using the command line ask user a question for students name
+# Using the command line ask user a question for assignment name
+# Using the command line ask user a question for grade in numbers
 
 
-user = Grader.new("John", "Grader", 102)
+
+user = Grader.new("John", "Grader", 94)
+# Return the letter grade for the assignment
 puts("This is " + user.getGrade())
 
-
-#   def getGrade
-#     if 0 > num:
-#         return "not a valid grade."
-#     elsif num < 60
-#       return "F"
-#     elsif num <= 69
-#       return "D"
-#     elsif num <= 79
-#       return "C"
-#     elsif num <= 89
-#       return "B"
-#     elsif num <= 100
-#       return "A"
-#     else
-#       return "not a valid grade."
-#     end
-#   end
-# end
-# end
-# end
-# end
+# To test your function, try it with these 5 point values and echo the result back out from the value returned from the function:
+# 1. 94
+# 2. 54
+# 3. 89.9
+# 4. 60.01
+# 5. 102.1
