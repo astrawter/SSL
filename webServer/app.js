ba027bcfc7db518995d81358d3dbdc27d494a2ef
Alexandria <afstrawter@gmail.com>
2020-05-24 16:28:33 -0500

    updated app.js
    stopped extra favicon call

--- webServer/app.js
+++ webServer/app.js
@@ -5,6 +5,12 @@ var url = require("url");
 
 
 http.createServer(function(req,res){
+  //stop the call for favicon
+  if (req.url === '/favicon.ico') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
 
   //get data from url
   var parsed = url.parse(req.url);
@@ -15,14 +21,30 @@ http.createServer(function(req,res){
   ext = filename.ext==""?".html":filename.ext;
   dir = filename.dir=="/"?"":filename.dir;
   page = filename.name==""?"index":filename.name;
+  console.log("dir: "+dir);
+  console.log("filen: "+filen);
+  console.log("ext: "+ext);
+  f = (dir+filen+ext).replace("/","");
+  console.log("f: "+f);
+  var mimeTypes = {
+    '.html':"text/html",
+    '.js':"text/javascript",
+    '.css':"text/css",
+    '.png':"image/png",
+    '.jpg':"image/jpg",
+    '.gif':"image/gif",
+  };
 
-  
-
-  fs.readFile(filen+".html",function(err,data){
-
-
-    res.writeHead(200);
-    res.end(data);
-  })
+  if (f) {
+    fs.readFile(f,function(err,data){
+// console.log("data: "+data);
+      if (page){
+        if (mimeTypes.hasOwnProperty(ext)){
+          res.writeHead(200, { 'Content-Type': ext});
+          res.end(data, 'utf-8');
+        }
+      }
+    })
+  }
 }).
 listen("8080")
