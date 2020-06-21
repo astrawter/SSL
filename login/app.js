32a8269cd56c986496897d5d1d6a63d3127fb179
Alexandria <afstrawter@gmail.com>
2020-06-20 23:47:50 -0500

    added bootstrap
    added bootstrap to login

--- login/app.js
+++ login/app.js
@@ -7,6 +7,7 @@ var request = require("request");
 var bodyParser = require("body-parser");
 
 var app = express();
+app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
